c93128a1a13a878f106ba395a5b39a38d58ea2d6
darshan275-f <darshanjomalingpatil@gmail.com>
2026-01-26 17:58:12 +0530

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import MechanicalSystems from './pages/MechanicalSystems';
 import ElectricalAutomation from './pages/ElectricalAutomation';
@@ -11,6 +12,7 @@ import Contact from './pages/Contact';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/mechanical-systems" element={<MechanicalSystems />} />
